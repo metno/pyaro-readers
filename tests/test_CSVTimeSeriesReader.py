b8264f6617d571f2ba2470f9d63aa029dfafa050
Jan Jurgen Griesfeller <jan.griesfeller@met.no>
2023-12-01 14:31:55 +0100

    linting

--- tests/test_CSVTimeSeriesReader.py
+++ tests/test_CSVTimeSeriesReader.py
@@ -8,10 +8,14 @@ from pyaro.timeseries.Wrappers import VariableNameChangingReader
 
 
 class TestCSVTimeSeriesReader(unittest.TestCase):
-    file = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                        'testdata', 'csvReader_testdata.csv')
+    file = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "testdata",
+        "csvReader_testdata.csv",
+    )
+
     def test_init(self):
-        engine = pyaro.list_timeseries_engines()['csv_timeseries']
+        engine = pyaro.list_timeseries_engines()["csv_timeseries"]
         self.assertEqual(engine.url(), "https://github.com/metno/pyaro")
         # just see that it doesn't fails
         engine.description()
@@ -24,8 +28,8 @@ class TestCSVTimeSeriesReader(unittest.TestCase):
             self.assertEqual(len(ts.stations()), 2)
 
     def test_stationfilter(self):
-        engine = pyaro.list_timeseries_engines()['csv_timeseries']
-        sfilter = pyaro.timeseries.filters.get('stations', exclude=['station1'])
+        engine = pyaro.list_timeseries_engines()["csv_timeseries"]
+        sfilter = pyaro.timeseries.filters.get("stations", exclude=["station1"])
         with engine.open(self.file, filters=[sfilter]) as ts:
             count = 0
             for var in ts.variables():
@@ -34,20 +38,24 @@ class TestCSVTimeSeriesReader(unittest.TestCase):
             self.assertEqual(len(ts.stations()), 1)
 
     def test_wrappers(self):
-        engine = pyaro.list_timeseries_engines()['csv_timeseries']
-        newsox = 'oxidised_sulphur'
-        with VariableNameChangingReader(engine.open(self.file, filters=[]),
-                                        {'SOx': newsox}) as ts:
+        engine = pyaro.list_timeseries_engines()["csv_timeseries"]
+        newsox = "oxidised_sulphur"
+        with VariableNameChangingReader(
+            engine.open(self.file, filters=[]), {"SOx": newsox}
+        ) as ts:
             self.assertEqual(ts.data(newsox).variable, newsox)
         pass
 
     def test_variables_filter(self):
-        engine = pyaro.list_timeseries_engines()['csv_timeseries']
-        newsox = 'oxidised_sulphur'
-        vfilter = pyaro.timeseries.filters.get('variables', reader_to_new={'SOx': newsox})
+        engine = pyaro.list_timeseries_engines()["csv_timeseries"]
+        newsox = "oxidised_sulphur"
+        vfilter = pyaro.timeseries.filters.get(
+            "variables", reader_to_new={"SOx": newsox}
+        )
         with engine.open(self.file, filters=[vfilter]) as ts:
             self.assertEqual(ts.data(newsox).variable, newsox)
         pass
+
 
 if __name__ == "__main__":
     unittest.main()
