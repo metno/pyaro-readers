2dd316b2f4dac4c814fb4a0991ca306806f03de1
Heiko Klein <heiko.klein@met.no>
2024-01-15 21:57:48 +0100

    cleaning up too many committed test-files

--- tests/test_Ascii2NetcdfTimeseries.py
+++ tests/test_Ascii2NetcdfTimeseries.py
@@ -6,35 +6,28 @@ import pyaro
 import pyaro.timeseries
 
 EBAS_URL = file = os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        "testdata",
-        "NILU"
-    )
+    os.path.dirname(os.path.realpath(__file__)), "testdata", "NILU"
+)
+
 
 class TestAscii2NetcdfTimeSeriesReader(unittest.TestCase):
     engine = "ascii2netcdf"
+
     def test_0engine(self):
         self.assertIn(self.engine, pyaro.list_timeseries_engines())
 
     def test_1open(self):
         with pyaro.open_timeseries(
-            self.engine,
-            EBAS_URL,
-            resolution="daily",
-            filters=[]
+            self.engine, EBAS_URL, resolution="daily", filters=[]
         ) as ts:
             self.assertGreater(len(ts.variables()), 70)
             self.assertGreater(len(ts.stations()), 300)
 
     def test_2read(self):
         with pyaro.open_timeseries(
-            self.engine,
-            EBAS_URL,
-            resolution="daily",
-            filters=[]
+            self.engine, EBAS_URL, resolution="daily", filters=[]
         ) as ts:
             data = ts.data("sulphur_dioxide_in_air")
             self.assertIn("AM0001", data.stations)
             self.assertGreater(np.sum(data.values), 10000)
             self.assertEqual(data.units, "ug")
-
---
+++ tests/testdata/NILU/data_daily.2021.nc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6d0411773ca01ae63eae59b2f72b2a3da495acfcf1955d72c5b565665528a782
+size 18636520
