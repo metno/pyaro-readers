79fdfa9f369381320909eed2c7bc908af83fc4f4
Heiko Klein <heiko.klein@met.no>
2024-01-15 18:31:40 +0100

    more testing

--- src/pyaro_readers/ascii2netcdf/Ascii2NetcdfTimeseries.py
+++ src/pyaro_readers/ascii2netcdf/Ascii2NetcdfTimeseries.py
@@ -10,24 +10,30 @@ from pyaro.timeseries import (
     Data,
     Flag,
     NpStructuredData,
-    Station
+    Station,
 )
 import pyaro.timeseries.Filter
 
 logger = logging.getLogger(__name__)
 
+
 class Ascii2NetcdfTimeseriesReaderException(Exception):
     pass
 
+
 class Ascii2NetcdfTimeseriesReader(AutoFilterReaderEngine.AutoFilterReader):
-    RESOLUTIONS = {60*60: 'hourly',
-                   60*60*24: 'daily',
-                   60*60*24*7: 'weekly',
-                   60*60*24*28: 'monthly',
-                   60*60*24*365: 'yearly'}
+    RESOLUTIONS = {
+        60 * 60: "hourly",
+        60 * 60 * 24: "daily",
+        60 * 60 * 24 * 7: "weekly",
+        60 * 60 * 24 * 28: "monthly",
+        60 * 60 * 24 * 365: "yearly",
+    }
+
     def __init__(
         self,
         filename,
+        resolution="daily",
         filters=[],
     ):
         """Initialize/open a new reader for netcdf-files converted from EBAS NASA-Ames-files
@@ -35,31 +41,41 @@ class Ascii2NetcdfTimeseriesReader(AutoFilterReaderEngine.AutoFilterReader):
 
         :param filename: directory name for data_daily.YYYY.nc, e.g.
             /lustre/storeB/project/fou/kl/emep/Auxiliary/NILU
+        :param resolution: hourly, daily, weekly, monthly, yearly
+            The resolutions are already merged in the conversion from ascii to netcdf, e.g. daily
+            contains the accumulated hourly data, too.
         :param filters: list of filters, defaults to []
         """
         self._set_filters(filters)
-        if os.path.is_directory(filename):
+        if os.path.isdir(filename):
             self._directory = filename
         else:
-            raise Ascii2NetcdfTimeseriesReaderException(f"no such file or directory: {filename}")
+            raise Ascii2NetcdfTimeseriesReaderException(
+                f"no such file or directory: {filename}"
+            )
 
-        dataglob = os.path.join(self._directory, "data_*.????.nc")
+        if resolution in self.RESOLUTIONS.values():
+            self._resolution = resolution
+        else:
+            raise Ascii2NetcdfTimeseriesReaderException(
+                f"unknown resolution: resolution"
+            )
+
+        dataglob = os.path.join(self._directory, f"data_{self._resolution}.????.nc")
         self._years = set()
         for file in glob.iglob(dataglob):
             year = file[-7:-3]
             if self._is_year_in_filters(year):
                 self._years.add(year)
 
-        self._resolutions = self.RESOLUTIONS.values() # TODO, pre-filter resolutions
-
         self._variables = self._read_file_variables()
-        station_file = 'StationList.csv'
+        station_file = "StationList.csv"
         station_filepath = os.path.join(self._directory, station_file)
-        if os.exists(station_filepath):
+        if os.path.exists(station_filepath):
             self._stations = self._read_station_list(station_filepath)
         else:
             dirname = os.path.basename(self._directory)
-            station_file = dirname + station_file # e.g. AirbaseStationList.csv
+            station_file = dirname + station_file  # e.g. AirbaseStationList.csv
             station_filepath = os.path.join(self._directory, station_file)
             if os.exists(station_filepath):
                 self._stations = self._read_station_list(station_filepath)
@@ -76,7 +92,7 @@ class Ascii2NetcdfTimeseriesReader(AutoFilterReaderEngine.AutoFilterReader):
         for fil in self._get_filters():
             if isinstance(fil, pyaro.timeseries.Filter.TimeBoundsFilter):
                 time_filter = fil
-        if fil.has_envelope():
+        if time_filter.has_envelope():
             start, end = time_filter.envelope()
             if end_year < start:
                 return False
@@ -86,123 +102,137 @@ class Ascii2NetcdfTimeseriesReader(AutoFilterReaderEngine.AutoFilterReader):
 
     def _read_file_variables(self):
         variables = {}
-        for resolution in self.RESOLUTIONS.values():
-            for year in self._years:
-                file = os.path.join(self._directory, f"data_{resolution}.{year}.nc")
-                if not os.path.exists(file):
-                    logger.info(f"no datafile for {year} and {resolution} at {file}, skipping...")
-                    continue
-                with netCDF4.Dataset(file, 'r') as nc:
-                    for vname, var in nc.variables.items():
-                        if vname.startswith('EPDL'):
-                            varname = f"{var.component}_in_{var.matrix}"
-                            units = "1"
-                            if 'units' in var.ncattrs():
-                                units = var.units
-                            if varname in variables:
-                                if units != variables[varname][1]:
-                                    raise Ascii2NetcdfTimeseriesReaderException(
-                                        f"units changed from {variables[varname][1]} to {units} for {varname}/{vname} in {file}"
-                                    )
-                            variables[varname] = (vname, units)
+        for year in self._years:
+            file = os.path.join(self._directory, f"data_{self._resolution}.{year}.nc")
+            if not os.path.exists(file):
+                logger.info(
+                    f"no datafile for {year} and {self._resolution} at {file}, skipping..."
+                )
+                continue
+            with netCDF4.Dataset(file, "r") as nc:
+                for vname, var in nc.variables.items():
+                    if vname.startswith("EPDL"):
+                        varname = f"{var.component}_in_{var.matrix}"
+                        units = "1"
+                        if "units" in var.ncattrs():
+                            units = var.units
+                        if varname in variables:
+                            if units != variables[varname][1]:
+                                logger.warning(
+                                    f"units changed from {variables[varname][1]} to {units} for {varname}/{vname} in {file}"
+                                )
+                        variables[varname] = (vname, units)
         return variables
 
     def _read_station_list(self, file):
         stations = {}
-        '#StationName	urban/suburban/rural	CountryName	ISO2	Latitude (degrees)	Longitude (degrees)	Altitude (m.a.s.l.)	LocationCode'
+        "#StationName	urban/suburban/rural	CountryName	ISO2	Latitude (degrees)	Longitude (degrees)	Altitude (m.a.s.l.)	LocationCode"
         with open(file, encoding="UTF-8", newline="") as fh:
-            with csv.reader(fh, delimiter="\t") as csvfile:
-                for row in csvfile:
-                    if not len(row):
-                        continue
-                    if row[0].startswith("#"):
-                        continue
-                    if len(row) < 7:
-                        logger.warning(r"missing elements in file {file}: {row}")
-                        continue
-                    if row[6] in stations:
-                        logger.warning(r"duplicated station in file {file}: {row[6]}")
-                    else:
-                        stations[row[6]] = Station(
-                            {
-                                "station": row[6],
-                                "longitude": float(row[4]),
-                                "latitude": float(row[3]),
-                                "altitude": float(row[5]),
-                                "country": row[2],
-                                "url": "",
-                                "long_name": row[0],
-                                # not used: rural/urban
-                            }
-                        )
+            csvfile = csv.reader(fh, delimiter="\t")
+            for row in csvfile:
+                if not len(row):
+                    continue
+                if row[0].startswith("#"):
+                    continue
+                if len(row) < 8:
+                    logger.warning(r"missing elements in file {file}: {row}")
+                    continue
+                if row[6] in stations:
+                    logger.warning(r"duplicated station in file {file}: {row[6]}")
+                else:
+                    stations[row[7]] = Station(
+                        {
+                            "station": row[7],
+                            "longitude": float(row[5]),
+                            "latitude": float(row[4]),
+                            "altitude": float(row[6]),
+                            "country": row[3],
+                            "url": "",
+                            "long_name": row[0],
+                            # not used: rural/urban
+                        }
+                    )
         return stations
 
     def _get_data_from_ncfile(self, varname, file, data):
-        with netCDF4.Dataset(file, 'r') as nc:
-            start_times = netCDF4.num2date(nc['time'][:], nc['time'].units)
-            end_times = start_times + (start_times[1]-start_times[0])
-            stations = nc['station'][:]
+        with netCDF4.Dataset(file, "r") as nc:
+            start_times = netCDF4.num2date(nc["time"][:], nc["time"].units)
+            end_times = start_times + (start_times[1] - start_times[0])
+            stations = nc["station"][:]
             (epdl, _) = self._variables[varname]
-            if not epdl in nc:
+            if not epdl in nc.variables:
                 return
             vdata = np.ma.filled(nc[epdl][:], np.nan)
             if nc[epdl].units != data.units:
-                logger.warning(f"units-change for {varname} in {file}: {nc[epdl].units} != {data.units}")
+                logger.warning(
+                    f"units-change for {varname} in {file}: {nc[epdl].units} != {data.units}"
+                )
 
             # get all arrays into same size, time fastes moving, i.e. [station][time]
             dstruct = {}
-            dstruct['start_times'] = np.tile(start_times, vdata.shape[0])
-            dstruct['end_times'] = np.tile(end_times, vdata.shape[0])
+            dstruct["start_times"] = np.tile(start_times, vdata.shape[0])
+            dstruct["end_times"] = np.tile(end_times, vdata.shape[0])
             lats = []
             lons = []
             alts = []
+            stats = []
             for i in range(stations.shape[0]):
                 station = str(netCDF4.chartostring(stations[i]))
                 if not station in self.stations():
                     lats.append(np.nan)
                     lons.append(np.nan)
                     alts.append(np.nan)
+                    stats.append(station)
                 else:
                     stat = self._stations[station]
                     lats.append(stat.latitude)
                     lons.append(stat.longitude)
                     alts.append(stat.altitude)
+                    stats.append(station)
             lat = np.array(lats)
             lon = np.array(lons)
             alt = np.array(alts)
-            dstruct['stations'] = np.repeat(stations, vdata.shape[1])
-            dstruct['lats'] = np.repeat(lat, vdata.shape[1])
-            dstruct['lons'] = np.repeat(lon, vdata.shape[1])
-            dstruct['alts'] = np.repeat(alt, vdata.shape[1])
-            dstruct['data'] = vdata.flatten()
+            dstruct["stations"] = np.repeat(stats, vdata.shape[1])
+            dstruct["lats"] = np.repeat(lat, vdata.shape[1])
+            dstruct["lons"] = np.repeat(lon, vdata.shape[1])
+            dstruct["alts"] = np.repeat(alt, vdata.shape[1])
+            dstruct["data"] = vdata.flatten()
 
             # filter out undefined data or stations
-            idx = np.isfinite(dstruct['data']) & np.isfinite(dstruct['lats'])
+            idx = np.isfinite(dstruct["data"]) & np.isfinite(dstruct["lats"])
             for key in dstruct.keys():
                 dstruct[key] = dstruct[key][idx]
 
-            dstruct['flags'] = dstruct['data'].astype('i4')
-            dstruct['flags'][:] = Flag.VALID
+            dstruct["flags"] = dstruct["data"].astype("i4")
+            dstruct["flags"][:] = Flag.VALID
 
-            data.append(dstruct['data'], dstruct['station'], dstruct['lats'],
-                        dstruct['lons'], dstruct['alts'], dstruct['start'], dstruct['end'],
-                        dstruct['flags'], dstruct['data']*np.nan)
+            data.append(
+                dstruct["data"],
+                dstruct["stations"],
+                dstruct["lats"],
+                dstruct["lons"],
+                dstruct["alts"],
+                dstruct["start_times"],
+                dstruct["end_times"],
+                dstruct["flags"],
+                dstruct["data"] * np.nan,
+            )
             return
-
 
     def _unfiltered_data(self, varname) -> Data:
         (_, units) = self._variables[varname]
-        data = Data.NpStructuredData(varname, units)
+        data = NpStructuredData(varname, units)
 
         for year in self._years:
-            for resolution in self._resolutions:
-                file = os.path.join(self._directory, f"data_{resolution}.{year}.nc")
-                if not os.path.exists(file):
-                    logger.info(f"no datafile for {year} and {resolution} at {file}, skipping...")
-                    continue
-                self._get_data_from_ncfile(varname, file, data)
+            file = os.path.join(self._directory, f"data_{self._resolution}.{year}.nc")
+            if not os.path.exists(file):
+                logger.info(
+                    f"no datafile for {year} and {self._resolution} at {file}, skipping..."
+                )
+                continue
+            self._get_data_from_ncfile(varname, file, data)
 
-        return self._data[varname]
+        return data
 
     def _unfiltered_stations(self) -> dict[str, Station]:
         return self._stations
@@ -214,7 +244,7 @@ class Ascii2NetcdfTimeseriesReader(AutoFilterReaderEngine.AutoFilterReader):
         pass
 
 
-class Ascii2NetcdfTimeseriesReaderEngine(AutoFilterReaderEngine.AutoFilterEngine):
+class Ascii2NetcdfTimeseriesEngine(AutoFilterReaderEngine.AutoFilterEngine):
     """Ascii-files converted by MSC-W to netcdf-format, e.g. using niluAscii2netcdf or eea_airquip2emepdata.py"""
 
     def reader_class(self):
--- tests/test_Ascii2NetcdfTimeseries.py
+++ tests/test_Ascii2NetcdfTimeseries.py
@@ -1,23 +1,39 @@
+import os
 import unittest
+import numpy as np
 
 import pyaro
 import pyaro.timeseries
 
-EBAS_URL = "/disk1/tmp/NILU/"
+EBAS_URL = file = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "testdata",
+        "NILU"
+    )
 
 class TestAscii2NetcdfTimeSeriesReader(unittest.TestCase):
     engine = "ascii2netcdf"
-    def test_engine(self):
+    def test_0engine(self):
         self.assertIn(self.engine, pyaro.list_timeseries_engines())
 
-    def test_open(self):
+    def test_1open(self):
         with pyaro.open_timeseries(
             self.engine,
             EBAS_URL,
             resolution="daily",
             filters=[]
         ) as ts:
-            self.assertGreater(len(ts.variables()), 0)
-            self.assertGreater(len(ts.stations()), 0)
+            self.assertGreater(len(ts.variables()), 70)
+            self.assertGreater(len(ts.stations()), 300)
 
-
+    def test_2read(self):
+        with pyaro.open_timeseries(
+            self.engine,
+            EBAS_URL,
+            resolution="daily",
+            filters=[]
+        ) as ts:
+            data = ts.data("sulphur_dioxide_in_air")
+            self.assertIn("AM0001", data.stations)
+            self.assertGreater(np.sum(data.values), 10000)
+            self.assertEqual(data.units, "ug")
