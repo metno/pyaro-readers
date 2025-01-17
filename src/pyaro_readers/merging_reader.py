98e54e33b74d9907d0040bcaac6f8b5ea2a1ed6e
Magnus Ulimoen <magnusu@met.no>
2025-01-17 15:18:23 +0100

    Add MergingReader

---
+++ src/pyaro_readers/merging_reader.py
@@ -0,0 +1,132 @@
+import logging
+from datetime import datetime, timedelta
+from pathlib import Path
+from typing import Literal, Any
+from collections.abc import Iterable
+import importlib.resources
+import dataclasses
+
+from tqdm import tqdm
+import numpy as np
+import cf_units
+import polars
+from pyaro.timeseries.AutoFilterReaderEngine import (
+    AutoFilterReader,
+    AutoFilterEngine,
+)
+from pyaro.timeseries import (
+    Reader,
+    Data,
+    Station,
+)
+import pyaro.timeseries
+
+
+class MergingReaderException(Exception):
+    pass
+
+
+class MergingReaderData(Data):
+    def __init__(self, data: list[Data], variable: str) -> None:
+        if len(data) == 0:
+            raise MergingReaderException("Requires at least one dataset")
+        self._data = data
+        self._variable = variable
+
+    @property
+    def units(self) -> str:
+        base_unit = self._data[0].units
+        unit = cf_units.Unit(base_unit)
+        for d in self._data[1:]:
+            if unit.convert_to(1, d.units) != 1.0:
+                raise MergingReaderException(
+                    f"The units are not the same in all the datasets {base_unit} {d.units}"
+                )
+        return base_unit
+
+    def keys(self):
+        raise NotImplementedError
+
+    def slice(self, index):
+        return MergingReaderData([d[index] for d in self._data], self._variable)
+
+    @property
+    def values(self) -> np.ndarray:
+        return np.concatenate([d.values for d in self._data])
+
+    @property
+    def stations(self) -> np.ndarray:
+        return np.concatenate([d.stations for d in self._data])
+
+    @property
+    def latitudes(self) -> np.ndarray:
+        return np.concatenate([d.latitudes for d in self._data])
+
+    @property
+    def longitudes(self) -> np.ndarray:
+        return np.concatenate([d.longitudes for d in self._data])
+
+    @property
+    def altitudes(self) -> np.ndarray:
+        return np.concatenate([d.altitudes for d in self._data])
+
+    @property
+    def start_times(self) -> np.ndarray:
+        return np.concatenate([d.start_times for d in self._data])
+
+    @property
+    def end_times(self) -> np.ndarray:
+        return np.concatenate([d.end_times for d in self._data])
+
+    @property
+    def flags(self) -> np.ndarray:
+        return np.concatenate([d.flags for d in self._data])
+
+    @property
+    def standard_deviations(self) -> np.ndarray:
+        return np.concatenate([d.standard_deviations for d in self._data])
+
+    def __len__(self) -> int:
+        return sum(len(d) for d in self._data)
+
+
+class MergingReader(AutoFilterReader):
+    def __init__(self, datasets: list[dict[str, Any]], filters=[]):
+        self._datasets = []
+        self._set_filters(filters)
+        for d in datasets:
+            readername = d.pop("readername")
+            self._datasets.append(
+                pyaro.open_timeseries(readername, **d, filters=self._get_filters())
+            )
+
+    def _unfiltered_data(self, varname: str) -> Data:
+        return MergingReaderData([d.data(varname) for d in self._datasets], varname)
+
+    def _unfiltered_stations(self) -> list[str]:
+        stations = []
+        for d in self._datasets:
+            stations.extend(d.stations())
+        return stations
+
+    def _unfiltered_variables(self) -> list[str]:
+        variables = []
+        for d in self._datasets:
+            variables.extend(d.variables())
+        return variables
+
+    def close(self):
+        for d in self._datasets:
+            d.close()
+
+
+class MergingReaderEngine(AutoFilterEngine):
+    def description(self) -> str:
+        return """Merge multiple datasets by concatenation of pyaro datasets
+        """
+
+    def url(self) -> str:
+        return "https://github.com/metno/pyaro-readers"
+
+    def reader_class(self) -> AutoFilterReader:
+        return MergingReader
---
+++ tests/test_merginreader.py
@@ -0,0 +1,21 @@
+import os
+
+import pyaro
+
+EBAS_URL = file = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "testdata", "NILU"
+)
+
+
+def test_stuff():
+    with pyaro.open_timeseries(
+        "mergingreader",
+        datasets=[
+            {"readername": "ascii2netcdf", "filename": EBAS_URL},
+            {"readername": "ascii2netcdf", "filename": EBAS_URL},
+        ],
+        filters=[],
+    ) as ts:
+        ts.variables()
+        ts.stations()
+        _data = ts.data("sulphur_dioxide_in_air")
