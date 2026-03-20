b72d664cb7d73ad1a044f088831e0dc2063531f9
Daniel Heinesen <danielh@met.no>
2026-03-20 21:16:39 +0100

    List of all ghost networks. To be revised when everything is downloaded

--- src/pyaro_readers/ghostreader/ghost_options.py
+++ src/pyaro_readers/ghostreader/ghost_options.py
@@ -31,4 +31,55 @@ EMISSION_SOURCE = [
 
 MEASUREMENT_METHODS = ["chemiluminescence (internal molybdenum converter)"]
 
-NETWORKS = ["EBAS-EMEP", "GHOST", "EEA_AQ_eReporting", "EBAS-ACTRIS", "US_EPA_AQS"]
+# NETWORKS = ["EBAS-EMEP", "GHOST", "EEA_AQ_eReporting", "EBAS-ACTRIS", "US_EPA_AQS"]
+
+
+# I think this is the full list of networks. Double check this when everything is downloaded
+NETWORKS = [
+    "AERONET_v3_lev1.5",
+    "AERONET_v3_lev2.0",
+    "CANADA_NAPS",
+    "CAPMoN",
+    "CHILE_SINCA",
+    "EANET",
+    "EBAS-ACTRIS",
+    "EBAS-AMAP",
+    "EBAS-CAMP",
+    "EBAS-COLOSSAL",
+    "EBAS-EMEP",
+    "EBAS-EUCAARI",
+    "EBAS-EUSAAR",
+    "EBAS-GUAN",
+    "EBAS-HELCOM",
+    "EBAS-HTAP",
+    "EBAS-IMPACTS",
+    "EBAS-IMPROVE",
+    "EBAS-Independent",
+    "EBAS-NILU",
+    "EBAS-NOAA_ESRL",
+    "EBAS-NOAA_GGGRN",
+    "EBAS-OECD",
+    "EBAS-RI_URBANS",
+    "EBAS-UK_DECC",
+    "EBAS-WMO_WDCA",
+    "EBAS-WMO_WDCRG",
+    "EEA",
+    "EEA_AIRBASE",
+    "EEA_AQ_eReporting",
+    "GHOST",
+    "GHOST-PUBLIC",
+    "INDAAF",
+    "MEXICO_CDMX",
+    "MITECO",
+    "UK_AIR",
+    "US_EPA_AQS",
+    "US_EPA_AirNow_DOS",
+    "US_EPA_CASTNET",
+    "US_NADP_AIRMoN",
+    "US_NADP_AMNet",
+    "US_NADP_AMoN",
+    "US_NADP_MDN",
+    "US_NADP_NTN",
+    "WMO_WDCGG",
+    "WMO_WDCPC",
+]
